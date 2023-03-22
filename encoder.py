85b1720542f24716e94d193a3e1f1cc91d7e3889
jayce <jaycecorcoran@gmail.com>
2023-03-22 18:32:42 -0400

    Decode

--- encoder.py
+++ encoder.py
@@ -12,19 +12,39 @@ def encode(digit):
         num = int(char)
         num += 3
         if num > 9:
-            num -=10
+            num -= 10
         password += str(num)
     return password
 
+
+def decode(password):
+    oldPass = ''
+    for x in password:
+        num = int(x)
+        if num == 0:
+            num = 7
+        elif num == 1:
+            num = 8
+        elif num == 2:
+            num = 9
+        else:
+            num = num - 3
+        oldPass += str(num)
+    return oldPass
+
+
 # Have main and encode
 def main():
+    newPass = ''
     while True:
         menu()
         choice = int(input("Please enter an option:"))
         if choice == 1:
             digit = (input("Please enter your password to encode:"))
-            encode(digit)
+            newPass = encode(digit)
             print("Your password has been stored and encoded!")
+        elif choice == 2:
+            print(f"The encoded password is {newPass} and the original password is " + decode(newPass))
         elif choice == 3:
             break
         else:
